e35e81a0824920b8a320576ff80f0e3541262de7
Fabian Kramm <fab.kramm@googlemail.com>
2025-08-07 13:26:58 +0200

    Update main.tf

--- node/main.tf
+++ node/main.tf
@@ -73,7 +73,7 @@ resource "aws_instance" "this" {
 
   # --- Root disk sizing ---
   root_block_device {
-    volume_size           = 50
+    volume_size           = 60
     volume_type           = "gp3"
     delete_on_termination = true
   }
